e942f868868d69fbfb736910a1af5a4b3a7543dd
yaswanthgitcode <yaswanthgiet@gmail.com>
2025-05-12 17:28:34 +0530

    Delete yt portfolio directory
    deleted

--- yt portfolio/script.js
+++
@@ -1,6 +0,0 @@
- const hamburger = document.querySelector(".hamburger");
-        const menu_bar = document.querySelector(".navbar-menu .manu-bar");
-
-        hamburger.addEventListener("click", () => {
-            menu_bar.classList.toggle("show");
-        })
